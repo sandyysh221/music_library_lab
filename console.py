ea891f77827cb4a8460d9de0e0a8332224cacc7a
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-06-07 19:39:00 +0100

    delete all artist/albums done

--- console.py
+++ console.py
@@ -12,4 +12,6 @@ album_1 = Album("24K Magic", "R&B", "Bruno Mars")
 music_repository.save(album_1)
 music_repository.select_all()
 
+artist_repository.delete_all()
+music_repository.delete_all()
 pdb.set_trace()
